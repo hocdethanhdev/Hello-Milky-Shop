b973fa5844fca81f5d454bf0aa177451c43a09bf
Thinhlp176 <phuocthinh17623@gmail.com>
2024-06-06 09:57:31 +0700

    upload them

--- FrontEnd/src/managers/staff/ProductDetailModal.js
+++ FrontEnd/src/managers/staff/ProductDetailModal.js
@@ -1,5 +1,8 @@
-import React from 'react';
+
 import './ProductDetailModal.css';
+
+
+
 
 const ProductDetailModal = ({ product, onClose }) => {
     if (!product) return null;
@@ -9,17 +12,23 @@ const ProductDetailModal = ({ product, onClose }) => {
             <div className="modal-content">
                 <span className="close-button" onClick={onClose}>&times;</span>
                 <h2>Product Details</h2>
-                <p><strong>ID:</strong> {product.ProductID}</p>
-                <p><strong>Name:</strong> {product.ProductName}</p>
-                <p><strong>Description:</strong> {product.Description}</p>
-                <p><strong>Price:</strong> {product.Price}</p>
-                <p><strong>Stock Quantity:</strong> {product.StockQuantity}</p>
-                <p><strong>Expiration Date:</strong> {new Date(product.ExpirationDate).toLocaleDateString()}</p>
-                <p><strong>Manufacturing Date:</strong> {new Date(product.ManufacturingDate).toLocaleDateString()}</p>
-                <p><strong>Brand:</strong> {product.BrandName}</p>
-                <p><strong>Category:</strong> {product.ProductCategoryName}</p>
-                <p><strong>Status:</strong> {product.Status ? 'Still in stock' : 'Out of stock'}</p>
-                <img src={product.Image} alt={product.ProductName} style={{ maxWidth: '100%' }} />
+                <div className='modal-thinh-anh'>
+                    <img src={product.Image} alt={product.ProductName} style={{ maxWidth: '100%' }} />
+                </div>
+                <div>
+
+                    <p><strong>ID:</strong> {product.ProductID}</p>
+                    <p><strong>Name:</strong> {product.ProductName}</p>
+                    <p><strong>Description:</strong> {product.Description}</p>
+                    <p><strong>Price:</strong> {product.Price}</p>
+                    <p><strong>Stock Quantity:</strong> {product.StockQuantity}</p>
+                    <p><strong>Expiration Date:</strong> {new Date(product.ExpirationDate).toLocaleDateString()}</p>
+                    <p><strong>Manufacturing Date:</strong> {new Date(product.ManufacturingDate).toLocaleDateString()}</p>
+                    <p><strong>Brand:</strong> {product.BrandName}</p>
+                    <p><strong>Category:</strong> {product.ProductCategoryName}</p>
+                    <p><strong>Status:</strong> {product.Status ? 'Still in stock' : 'Out of stock'}</p>
+                </div>
+
             </div>
         </div>
     );
