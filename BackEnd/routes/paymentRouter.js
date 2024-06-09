e093e8c95f12f2e69ba5d1b081fdc900dd12578c
N-I-V-Z <vothanhnhan106@gmail.com>
2024-06-09 22:43:01 +0700

    test payment return

--- BackEnd/routes/paymentRouter.js
+++ BackEnd/routes/paymentRouter.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const paymentController = require("../controller/paymentController");
+require('dotenv').config();
 
 router.post('/create_payment_url', paymentController.createVNPayPayment);
 
-router.get('/vnpay_return', paymentController.vnpayReturn)
+router.get('/vnpay_return', paymentController.vnpayReturn, (req, res) => {
+    res.redirect(`${process.env.CLIENT_URL}/ShoppingCarts`)
+})
 
 module.exports = router
 
