472b4a00e6aa2406249fa300db3d087b04b6c9db
N-I-V-Z <vothanhnhan106@gmail.com>
2024-06-13 12:11:56 +0700

    add forgetPassword

--- BackEnd/controller/authController.js
+++ BackEnd/controller/authController.js
@@ -1,31 +1,58 @@
 const authService = require("../service/authService");
-const otpService = require("../service/otpService");
-const passport = require('passport');
-const User = require('../bo/user');
-const jwt = require('jsonwebtoken');
+const passport = require("passport");
+const User = require("../bo/user");
+const jwt = require("jsonwebtoken");
+
+const forgetPassword = async (req, res) => {
+  try {
+    const { Password, UserID } = req.body;
+    const hashedPassword = await authService.hashPassword(Password);
+    const obj = await authService.forgetPassword(hashedPassword, UserID);
+    res.send(obj);
+  } catch (error) {
+    console.error("Error in forgetPassword controller:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+const checkPhoneNumber = async (req, res) => {
+  try {
+    const { PhoneNumber } = req.body;
+    const obj = await authService.checkPhoneNumber(PhoneNumber);
+    res.send(obj);
+  } catch (error) {
+    console.error("Error in checkPhoneNumber controller:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 const login = async (req, res) => {
-    try {
-      const obj = await authService.login(req.body);
-      res.send(obj);
-    } catch (error) {
-      console.error("Error while logging in:", error);
-      res.status(500).send("Internal Server Error");
-    }
-  };
+  try {
+    const obj = await authService.login(req.body);
+    res.send(obj);
+  } catch (error) {
+    console.error("Error while logging in:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 let checkLoggedOut = (req, res) => {
-    if(req.isAuthenticated()){
-        return res.redirect("/");
-    }
-    next();
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
 };
 
 const register = async (req, res) => {
   try {
     const { UserName, PhoneNumber, Password, RoleID } = req.body;
     const hashedPassword = await authService.hashPassword(Password);
-    const obj = await authService.register(UserName, PhoneNumber, hashedPassword, RoleID);
+    const obj = await authService.register(
+      UserName,
+      PhoneNumber,
+      hashedPassword,
+      RoleID
+    );
     res.send(obj);
   } catch (error) {
     console.error("Error while registering:", error);
@@ -47,24 +74,27 @@ const logout = async (req, res) => {
 const loginEmail = async (req, res) => {
   const { email } = req?.body;
   try {
-    if (!email) res.status(404).json({
-      err: 1,
-      message: 'Missing input'
-    })
+    if (!email)
+      res.status(404).json({
+        err: 1,
+        message: "Missing input",
+      });
     const obj = await authService.loginEmail(email);
     res.status(200).json(obj);
   } catch (error) {
     res.status(500).json({
       err: -1,
-      message: "Internal Server Error"
+      message: "Internal Server Error",
     });
   }
 };
 
 module.exports = {
-    checkLoggedOut,
-    login,
-    register,
-    logout,
-    loginEmail,
-}
+  checkLoggedOut,
+  login,
+  register,
+  logout,
+  loginEmail,
+  checkPhoneNumber,
+  forgetPassword,
+};
