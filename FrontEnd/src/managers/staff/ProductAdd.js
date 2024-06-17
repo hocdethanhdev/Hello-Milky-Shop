5af5fdcdcbe8836c685cfe42b3fe290e20f5cc9f
hocdethanhdev <tritrann03@gmail.com>
2024-06-17 22:08:03 +0700

    Update staff, user

--- FrontEnd/src/managers/staff/ProductAdd.js
+++ FrontEnd/src/managers/staff/ProductAdd.js
@@ -109,7 +109,7 @@ const ProductAdd = () => {
 
   return (
     <div className="create-product">
-      <h2>Create New Product</h2>
+     
       {successMessage && (
         <p
           className={`success-message ${
