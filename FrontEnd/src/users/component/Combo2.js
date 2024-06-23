fbd1a97a0c0bd90d7fb8ff3c2f025bf7230e5078
N-I-V-Z <vothanhnhan106@gmail.com>
2024-06-23 15:31:15 +0700

    fix combo

--- FrontEnd/src/users/component/Combo2.js
+++ FrontEnd/src/users/component/Combo2.js
@@ -1,66 +1,96 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import "./Product1.css";
 import StarRating from "../ui-list-product-mom/StarRating";
 function Combo2() {
-    const [products, setProducts] = useState([]);
-    const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        fetch('http://localhost:5000/api/v1/product/getTop6MilkForBaby')
-            .then(response => response.json())
-            .then(data => setProducts(data))
-            .catch(error => console.error('Error fetching data:', error));
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/v1/product/getTop6MilkForBaby")
+      .then((response) => response.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
-    const formatPrice = (price) => {
-        return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(price);
-    };
+  const formatPrice = (price) => {
+    return new Intl.NumberFormat("vi-VN", {
+      style: "currency",
+      currency: "VND",
+    }).format(price);
+  };
 
-    const handleProductClick = (productId) => {
-        navigate(`/product/${productId}`);
-    };
+  const handleProductClick = (productId) => {
+    navigate(`/product/${productId}`);
+  };
 
-    return (
-        <section id="combo_2f" className="combo-2f-section">
-            <div className="combo-2f-wrap">
-                <div className="combo-2f-content">
-                    <div className="combo-2f-header">
-                        <div className="combo-2f-icon">
-                            <img src="https://momslove.com.vn/wp-content/uploads/2021/07/icon-sua.svg" alt="Combo icon" />
-                        </div>
-                        <div className="combo-2f-title">
-                            <h2>Sữa dành cho bé</h2>
-                        </div>
-                    </div>
-                    <div className="combo-2f-main">
-                        <img src="./ImageMilkShop/banner-tang.png" alt="Combo siêu tiết kiệm" />
-                    </div>
-                    <div className="combo-content-1">
-                        {products.map((product, index) => (
-                            <div key={product.ProductID} className="combo-item-1" onClick={() => handleProductClick(product.ProductID)}>
-                                <img src={product.Image} alt={product.ProductName} />
-                                <div className="combo-details-1">
-                                    <h3>{product.ProductName}</h3>
-                                    {product.PriceAfterDiscounts !== product.Price ? (
-                                        <div className="gia">
-                                        <p className="original-price-1">{formatPrice(product.Price)}</p>
-                                            <p className="discounted-price-1">{formatPrice(product.PriceAfterDiscounts)}</p>
-                                            
-                                        </div>
-                                    ) : (
-                                        <p>{formatPrice(product.Price)}</p>
-                                    )}
-                                    <div className='saoduoithinh-1'><StarRating productId={product.ProductID}/></div>
-                                </div>
-                                
-                            </div>
-                        ))}
-                    </div>
-                </div>
+  const formatPriceDiscount = (price) => {
+    return `${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")}`;
+  };
+
+  return (
+    <section id="combo_2f" className="combo-2f-section">
+      <div className="combo-2f-wrap">
+        <div className="combo-2f-content">
+          <div className="combo-2f-header">
+            <div className="combo-2f-icon">
+              <img
+                src="https://momslove.com.vn/wp-content/uploads/2021/07/icon-sua.svg"
+                alt="Combo icon"
+              />
             </div>
-        </section>
-    );
+            <div className="combo-2f-title">
+              <h2>Sữa dành cho bé</h2>
+            </div>
+          </div>
+          <div className="combo-2f-main">
+            <img
+              src="./ImageMilkShop/banner-tang.png"
+              alt="Combo siêu tiết kiệm"
+            />
+          </div>
+          <div className="combo-content-1">
+            {products.map((product, index) => (
+              <div
+                key={product.ProductID}
+                className="combo-item-1"
+                onClick={() => handleProductClick(product.ProductID)}
+              >
+                <img src={product.Image} alt={product.ProductName} />
+                <div className="combo-details-1">
+                  <h3>{product.ProductName}</h3>
+                  <div className="saoduoithinh-1">
+                    <StarRating productId={product.ProductID} />
+                  </div>
+                  {product.PriceAfterDiscounts !== product.Price ? (
+                    <div className="gia">
+                      <p className="discounted-price-1">
+                        {formatPrice(product.PriceAfterDiscounts)}
+                      </p>
+                      <p className="original-price-1">
+                        {formatPrice(product.Price)}
+                      </p>
+                    </div>
+                  ) : (
+                    <p>{formatPrice(product.Price)}</p>
+                  )}
+                </div>
+                {product.Price !== product.PriceAfterDiscounts && (
+                  <span className="discount">
+                    -
+                    {formatPriceDiscount(
+                      (product.Price - product.PriceAfterDiscounts) / 1000
+                    )}
+                    K
+                  </span>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Combo2;
