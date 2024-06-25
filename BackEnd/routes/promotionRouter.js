d06bb7cd3c1dcc3bd226a9f1f1e6a0081aa87522
nonglongnhong <long041103@gmail.com>
2024-06-25 20:49:18 +0700

    add deletePromotion

--- BackEnd/routes/promotionRouter.js
+++ BackEnd/routes/promotionRouter.js
@@ -18,6 +18,8 @@ router.get('/getCurrentProductsHavingPromotion', promotionController.getCurrentP
 
 router.get('/getPromotionByDate', promotionController.getPromotionByDate)
 
+router.put('/deletePromotion/:promotion_id', promotionController.deletePromotion);
+
 module.exports = router
 
 
