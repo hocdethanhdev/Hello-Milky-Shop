e2499251eb496d8d7fe1c33efb360e366ce5578a
N-I-V-Z <vothanhnhan106@gmail.com>
2024-06-30 11:32:43 +0700

    vnpay fix

--- BackEnd/routes/paymentRouter.js
+++ BackEnd/routes/paymentRouter.js
@@ -1,14 +1,13 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const paymentController = require("../controller/paymentController");
-require('dotenv').config();
+require("dotenv").config();
 
-router.post('/create_payment_url', paymentController.createVNPayPayment);
+router.post("/create_payment_url", paymentController.createVNPayPayment);
 
-router.get('/vnpay_return', paymentController.vnpayReturn, (req, res) => {
-  res.redirect(`${process.env.CLIENT_URL}/PaymentSuccess?status=${req.result.status}&code=${req.result.code}`);
+router.get("/vnpay_return", paymentController.vnpayReturn, (req, res) => {
+  res.redirect(
+    `${process.env.CLIENT_URL}/PaymentSuccess?status=${req.result.status}&code=${req.result.code}`
+  );
 });
 
-module.exports = router
-
-
-
+module.exports = router;
