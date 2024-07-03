cffea84e75613568a2658b7fa0c7e1cf09f5d6ca
N-I-V-Z <vothanhnhan106@gmail.com>
2024-07-03 18:23:08 +0700

    fix err upload image

--- FrontEnd/src/components/managers/richtext/RichTextEditor.js
+++ FrontEnd/src/components/managers/richtext/RichTextEditor.js
@@ -1,46 +1,19 @@
-import React, { useRef, useMemo } from "react";
-import PropTypes from "prop-types"; // Import PropTypes
+import React, { useState } from 'react';
+import RichTextEditor from './RichTextEditor';
 
-import JoditEditor from "jodit-react";
+function App() {
+  const [editorContent, setEditorContent] = useState('');
 
-function RichTextEditor({ value, onChange }) {
-  const editor = useRef(null);
-
-  const config = useMemo(() => ({
-    readonly: false,
-    toolbar: true,
-    toolbarButtonSize: 'middle',
-    toolbarSticky: false,
-    showCharsCounter: false,
-    showWordsCounter: false,
-    showXPathInStatusbar: false,
-    buttons: [
-      'bold', 'italic', 'underline', 'strikethrough', 'eraser',
-      '|', 'ul', 'ol', 'indent', 'outdent',
-      '|', 'font', 'fontsize', 'brush', 'paragraph',
-      '|', 'image', 'link', 'table',
-      '|', 'align', 'undo', 'redo', 'hr',
-      '|', 'copyformat', 'fullsize'
-    ]
-  }), []);
+  const handleEditorChange = (newContent) => {
+    setEditorContent(newContent);
+  };
 
   return (
-    <div>
-      <h1>Welcome to Ageee Dev</h1>
-      <JoditEditor
-        ref={editor}
-        value={value}
-        config={config}
-        onChange={(newContent) => onChange(newContent)}
-      />
+    <div className="App">
+      <h1>My Rich Text Editor</h1>
+      <RichTextEditor value={editorContent} onChange={handleEditorChange} />
     </div>
   );
 }
 
-// PropTypes validation
-RichTextEditor.propTypes = {
-  value: PropTypes.string, // Assuming value is a string
-  onChange: PropTypes.func.isRequired, // onChange is a required function prop
-};
-
-export default RichTextEditor;
+export default App;
