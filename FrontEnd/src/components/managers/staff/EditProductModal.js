a07986710c3e316bb32c699eaf6d9d5fc338c6d8
Thinhlp176 <phuocthinh17623@gmail.com>
2024-07-04 00:09:11 +0700

    fix bug in some files

--- FrontEnd/src/components/managers/staff/EditProductModal.js
+++ FrontEnd/src/components/managers/staff/EditProductModal.js
@@ -217,11 +217,6 @@ const EditProductModal = () => {
     () => ({
       readonly: false,
       toolbar: true,
-      toolbarButtonSize: "middle",
-      toolbarSticky: false,
-      showCharsCounter: false,
-      showWordsCounter: false,
-      showXPathInStatusbar: false,
       buttons: [
         "bold",
         "italic",
