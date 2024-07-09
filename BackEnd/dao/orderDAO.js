c5d44e225867f9345a392eb74f359a280100ad58
khaiqui <nguyenkhaiqui2003@gmail.com>
2024-07-10 00:02:56 +0700

    Qui fixes filter in dashboard

--- BackEnd/dao/orderDAO.js
+++ BackEnd/dao/orderDAO.js
@@ -33,7 +33,9 @@ const orderDAO = {
         request.query(
           `SELECT count(OrderID) as count
           FROM Orders o
-          WHERE Status = 1;`,
+          WHERE Status = 1
+          AND YEAR(OrderDate) = YEAR(GETUTCDATE()) 
+          AND MONTH(OrderDate) = MONTH(GETUTCDATE());`,
           (err, res) => {
             if (err) reject(err);
 
@@ -174,8 +176,8 @@ const orderDAO = {
           `SELECT COUNT(OrderID) as count
           FROM Orders 
           WHERE Status = 1 
-          AND YEAR(OrderDate) = YEAR(GETUTCDATE()) 
-          AND MONTH(OrderDate) = MONTH(GETUTCDATE());`,
+          AND CAST(OrderDate AS DATE) = CAST(GETUTCDATE() AS DATE);
+          `,
           (err, res) => {
             if (err) reject(err);
 
