62c1521cf5617a55337acfb67561d0e0afd43e23
hocdethanhdev <tritrann03@gmail.com>
2024-07-11 21:10:03 +0700

    update css language

--- FrontEnd/src/components/users/language/LanguageSelector.tsx
+++ FrontEnd/src/components/users/language/LanguageSelector.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
-import us from "../../../assests/US.svg"; 
+import React, { useState, useEffect, useRef } from "react";
+import us from "../../../assests/US.svg";
 import vn from "../../../assests/VN.svg";
 import down from "../../../assests/chevron-down.svg";
-import { useTranslation } from 'react-i18next';
-import './LanguageSelector.css'; // Assuming you have this CSS file
+import { useTranslation } from "react-i18next";
+import "./LanguageSelector.css"; // Assuming you have this CSS file
 
 interface LanguageSelectorProps {
   // No props passed in this example
@@ -21,30 +21,33 @@ const LanguageSelector: React.FC<LanguageSelectorProps> = () => {
   };
 
   const handleChoose = (lang: string) => {
-    const currentLang = lang === us ? 'en' : 'vi';
+    const currentLang = lang === us ? "en" : "vi";
     i18n.changeLanguage(currentLang);
-    localStorage.setItem('lang', currentLang);
+    localStorage.setItem("lang", currentLang);
     setLang(lang);
     setIsOpen(false);
   };
 
   const handleClickOutside = (event: MouseEvent) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
       setIsOpen(false);
     }
   };
 
   useEffect(() => {
-    const curLang = localStorage.getItem('lang');
+    const curLang = localStorage.getItem("lang");
     if (curLang) {
-      setLang(curLang === 'vi' ? vn : us);
+      setLang(curLang === "vi" ? vn : us);
     }
   }, []);
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
@@ -53,37 +56,46 @@ const LanguageSelector: React.FC<LanguageSelectorProps> = () => {
       setIsOpen(false);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <div className="relative inline-flex flex-row-reverse" ref={dropdownRef}>
+    <div
+      className="translate relative inline-flex flex-row-reverse"
+      ref={dropdownRef}>
       <button
-        className="py-2 px-4 inline-flex items-center gap-2 font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 w-28 mr-2"
-        onClick={toggleMenu}
-      >
-        <img className="w-5 h-auto rounded-full" src={lang} alt="current language" />
-        <span className="font-medium truncate">{lang === vn ? 'VI' : 'US'}</span>
-        <img src={down} className={`w-4 transform ${isOpen ? 'rotate-180' : ''}`} alt="chevron-down" />
+        className="language-selector-button-1 py-2 px-4 inline-flex items-center gap-2 font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 w-28 mr-2"
+        onClick={toggleMenu}>
+        <img
+          className="w-5 h-auto rounded-full"
+          src={lang}
+          alt="current language"
+        />
+        <span className="font-medium truncate">
+          {lang === vn ? "VI" : "US"}
+        </span>
+        <img
+          src={down}
+          className={`w-4 transform ${isOpen ? "rotate-180" : ""}`}
+          alt="chevron-down"
+        />
       </button>
 
       {isOpen && (
         <div className="absolute top-full right-0 w-40 bg-white shadow-lg rounded-lg py-2 mt-1 z-50">
           <button
-            className="flex items-center gap-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 w-full"
-            onClick={() => handleChoose(us)}
-          >
+            className="language-selector-button flex items-center gap-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none  w-full"
+            onClick={() => handleChoose(us)}>
             <img src={us} className="w-4 rounded-full" alt="English (US)" />
             <span>English (US)</span>
           </button>
           <button
-            className="flex items-center gap-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 w-full"
-            onClick={() => handleChoose(vn)}
-          >
+            className="language-selector-button flex items-center gap-3 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 w-full"
+            onClick={() => handleChoose(vn)}>
             <img src={vn} className="w-4 rounded-full" alt="Việt Nam (VI)" />
             <span>Việt Nam (VI)</span>
           </button>
