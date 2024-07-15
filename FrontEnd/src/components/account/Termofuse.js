079c6751d6dd5866250f10b398c0166de4d1c8f2
nonglongnhong <long041103@gmail.com>
2024-07-15 23:38:07 +0700

    Change language

--- FrontEnd/src/components/account/Termofuse.js
+++ FrontEnd/src/components/account/Termofuse.js
@@ -1,220 +1,146 @@
 import React from "react";
 import "./Termofuse.css";
+import { useTranslation } from 'react-i18next';
 
 function Termofuse() {
+  const { t } = useTranslation();
   return (
     <main id="main" className="main-container-termofuse">
       <div className="row page-wrapper">
         <div id="content" className="large-12 col" role="main">
           <header className="entry-header text-center">
-            <h1 className="entry-title">Điều khoản mua bán hàng hóa</h1>
+            <h1 className="entry-title">{t('termOf')}</h1>
             <div className="is-divider medium"></div>
           </header>
           <div className="entry-content">
             <p>
-              Khi quý khách truy cập vào trang web của chúng tôi có nghĩa là quý
-              khách đồng ý với các điều khoản này.
+            {t('whenYouVisit')}
             </p>
             <p>
-              Trang web có quyền thay đổi, chỉnh sửa, thêm hoặc lược bỏ bất kỳ
-              phần nào trong Quy định và Điều kiện sử dụng, vào bất cứ lúc nào.
-              Các thay đổi có hiệu lực ngay khi được đăng trên trang web mà không
-              cần thông báo trước.
+            {t('theWebsiiteReserves')}
             </p>
             <p>
-              Và khi quý khách tiếp tục sử dụng trang web, sau khi các thay đổi
-              về Quy định và Điều kiện được đăng tải, có nghĩa là quý khách chấp
-              nhận với những thay đổi đó.
+            {t('andWhenYouContinue')}
             </p>
-            <p>Quý khách vui lòng kiểm tra thường xuyên để cập nhật những thay đổi của chúng tôi. Xin vui lòng đọc kỹ trước khi quyết định mua hàng:</p>
+            <p>{t('pleaseCheckBack')}</p>
             <p>
-              <strong>1. Hướng dẫn sử dụng web</strong>
-              &#8211;
+              <strong>{t('webManual')}</strong>
             </p>
             <p>
-              Khi vào web của chúng tôi, người dùng tối thiểu phải 18 tuổi hoặc
-              truy cập dưới sự giám sát của cha mẹ hay người giám hộ hợp pháp.
+            {t('whenAccessing')}
             </p>
             <p>
-              &#8211;Nghiêm cấm sử dụng bất kỳ phần nào của trang web này với mục
-              đích thương mại hoặc nhân danh bất kỳ đối tác thứ ba nào nếu không
-              được chúng tôi cho phép bằng văn bản. Nếu vi phạm bất cứ điều nào
-              trong đây, chúng tôi sẽ hủy giấy phép của bạn mà không cần báo
-              trước.
+              &#8211;{t('itIsStricly')}
             </p>
             <p>
-              &#8211;Trang web này chỉ dùng để cung cấp thông tin sản phẩm chứ
-              chúng tôi không phải nhà sản xuất.
+              &#8211;{t('thisWebsite')}
             </p>
             <p>
-              &#8211;Quý khách phải đăng ký tài khoản với thông tin xác thực về
-              bản thân và phải cập nhật nếu có bất kỳ thay đổi nào. Mỗi người
-              truy cập phải có trách nhiệm với mật khẩu, tài khoản và hoạt động
-              của mình trên web.
+              &#8211;{t('youMustRegister')}
             </p>
             <p>
-              Hơn nữa, quý khách phải thông báo cho chúng tôi biết khi tài khoản
-              bị truy cập trái phép. Chúng tôi không chịu bất kỳ trách nhiệm nào,
-              dù trực tiếp hay gián tiếp, đối với những thiệt hại hoặc mất mát gây
-              ra do quý khách không tuân thủ quy định.
+            {t('inAddition')}
             </p>
             <p>
-              <strong>2. Ý kiến khách hàng</strong>
+              <strong>{t('customerOpinion')}</strong>
             </p>
             <p>
-              &#8211;Tất cả nội dung trang web và ý kiến phê bình của quý khách
-              đều là tài sản của chúng tôi. Nếu chúng tôi phát hiện bất kỳ thông
-              tin giả mạo nào, chúng tôi sẽ khóa tài khoản của quý khách ngay lập
-              tức hoặc áp dụng các biện pháp khác theo quy định của pháp luật
-              Việt Nam.
+              &#8211;{t('allWebsiteContent')}
             </p>
             <p>
-              <strong>3 &nbsp;Đặt hàng và xác nhận đơn hàng</strong>
-              &#8211;Khi quý khách đặt hàng tại Shopmebi.com, chúng tôi sẽ nhận
-              được yêu cầu đặt hàng và gửi đến quý khách Tuy nhiên, yêu cầu đặt
-              hàng cần thông qua một bước xác nhận đơn hàng, Shopmebi.com,&nbsp;chỉ
-              xác nhận đơn hàng nếu yêu cầu đặt hàng của quý khách thỏa mãn các
-              tiêu chí thực hiện đơn hàng tại &nbsp;Shopmebi.com.
+              <strong>3.&nbsp;{t('orderAndOrderComfirmation')}</strong><br/>
+              
+              &#8211;{t('whenYouPlace')} &nbsp;HelloMilky.shop.
             </p>
             <p>
-              &#8211;Để yêu cầu đặt hàng được xác nhận nhanh chóng, quý khách vui
-              lòng cung cấp đúng và đầy đủ các thông tin liên quan đến việc giao
-              nhận, hoặc các điều khoản và điều kiện của chương trình khuyến mãi
-              (nếu có) mà quý khách tham gia.
+              &#8211;{t('inOrderTo')}
             </p>
             <p>
-              <strong>4. Giá trị đơn hàng và hình thức thanh toán</strong>
+              <strong>{t('orderValue')}</strong>
             </p>
             <p>
-              &#8211;Shopmebi.com cung cấp các hình thức thanh toán: tiền mặt khi
-              nhận hàng, thanh toán trực tiếp cho ship, thanh toán tại cửa hàng,
-              thanh toán qua chuyển khoản ngân hàng.
+              &#8211;{t('helloMilkyShop')}
             </p>
             <p>
-              &#8211;Trừ một số trường hợp có ghi chú riêng, thông thường quý khách
-              có thể lựa chọn một trong 3 hình thức thanh toán trên khi tiến hành
-              đặt hàng.
+              &#8211;{t('unless')}
             </p>
             <p>
-              <strong>5. Chương trình khuyến mãi</strong>
+              <strong>{t('promotions')}</strong>
             </p>
             <p>
-              &#8211;Thể lệ của chương trình khuyến mãi sẽ được cập nhật tại trang
-              khuyến mãi theo từng thời điểm chạy chương trình và có thể được thay
-              đổi mà không cần thông báo trước cho khách hàng.
+              &#8211;{t('theRulesOfThePromotion')}
             </p>
             <p>
-              &#8211;Shopmebi.com có quyền từ chối các đơn hàng không thỏa mãn
-              điều khoản và điều kiện tham gia các chương trình khuyến mãi mà không
-              cần thông báo đến khách hàng. Vì vậy, xin quý khách vui lòng tham
-              khảo kĩ Thể lệ của từng chương trình trước khi tham gia.
+              &#8211;{t('HelloMilky.shop')}
             </p>
             <p>
-              <strong>6. Mã giảm giá</strong>
+              <strong>{t('voucher')}</strong>
             </p>
             <p>
-              &#8211;Mã giảm giá là hình thức chiết khấu mà Shopmebi.com dành cho
-              khách hàng có thể có giá trị giảm một phần hoặc toàn phần giá trị của
-              đơn hàng.
+              &#8211;{t('aDiscountCode')}
             </p>
             <p>
-              &#8211;Mã giảm giá có thể được Shopmebi.com cấp cho từng khách hàng
-              riêng biệt, hoặc phổ thông cho tất cả khách hàng tham gia vào chương
-              trình. Điều kiện của từng mã giảm giá sẽ được quy định rõ ràng từng
-              chương trình, và chỉ có giá trị khi khách hàng sử dụng đúng thời hạn,
-              cách sử dụng đã được quy định.
+              &#8211;{t('discountCodes')}
             </p>
             <p>
-              &#8211;Shopmebi.com có quyền từ chối các yêu cầu sử dụng mã giảm giá
-              không đáp ứng yêu cầu sử dụng của chương trình khuyến mãi mà không
-              cần thông báo trước cho khách hàng.
+              &#8211;{t('toTheCustomer')}
             </p>
             <p>
-              <strong>7. Bảo mật thông tin</strong>
+              <strong>{t('informationSercurity')}</strong>
             </p>
             <p>
-              &#8211;Tại Shopmebi.com, chúng tôi luôn cam kết bảo vệ quyền riêng
-              tư thông tin của bạn một cách tối đa. Tuy nhiên, bạn vui lòng đọc
-              kỹ những điều khoản trong Quy định và Điều kiện để hiểu rõ hơn về
-              chính sách bảo mật của chúng tôi.
+              &#8211;{t('atHelloMilkyShop')}
             </p>
             <p>
-              &#8211;Shopmebi.com có quyền lưu trữ lại thông tin cá nhân và giao
-              dịch của khách hàng để phục vụ mục đích điều tra, giải quyết khiếu
-              nại, quản lý chương trình khách hàng, tuy nhiên chúng tôi sẽ không
-              tiết lộ các thông tin này ra ngoài nếu không có sự đồng ý từ phía
-              khách hàng.
+              &#8211;{t('storePersonalInformation')}
             </p>
             <p>
-              &#8211;Mọi hình thức tác quyền nhân thân và tài khoản trong tài khoản
-              của khách hàng khi đăng ký có trách nhiệm với mật khẩu, tài khoản
-              và hoạt động trên trang web của chúng tôi.
+              &#8211;{t('allFormOfPersonal')}
             </p>
             <p>
-              <strong>8. Bảo hành sản phẩm</strong>
+              <strong>{t('productWarranty')}</strong>
             </p>
             <p>
-              &#8211;Với mỗi sản phẩm bạn mua, bạn được hưởng quyền lợi bảo hành
-              về các lỗi kĩ thuật, nếu sản phẩm có lỗi kĩ thuật, bạn sẽ được bảo
-              hành miễn phí, thay thế và sửa chữa sản phẩm. Thời hạn của sản phẩm
-              phụ thuộc vào chế độ của chúng tôi.
+              &#8211;{t('withEachProduct')}
             </p>
             <p>
-              &#8211;Shopmebi.com không chịu trách nhiệm trước một số lỗi kỹ thuật
-              xảy ra không liên quan đến sản phẩm. Tuy nhiên, chúng tôi sẽ không
-              chịu trách nhiệm trước một số lỗi kỹ thuật xảy ra không liên quan
-              đến sản phẩm.
+              &#8211;{t('isNotResponsible')}
             </p>
             <p>
-              <strong>9. Thay đổi và hủy đơn hàng</strong>
+              <strong>{t('changes')}</strong>
             </p>
             <p>
-              &#8211;Quý khách muốn hủy đơn hàng, xin vui lòng liên hệ với chúng
-              tôi qua hotline của chúng tôi hoặc email của chúng tôi để được hỗ
-              trợ.
+              &#8211;{t('ifYouWant')}
             </p>
             <p>
-              &#8211;Chúng tôi sẽ giúp quý khách giải quyết các vấn đề khác như
-              sản phẩm bị thiếu hoặc sản phẩm bị lỗi trong quá trình vận chuyển
-              tới tay quý khách.
+              &#8211;{t('weWillHelpYou')}
             </p>
             <p>
-              <strong>10. Cập nhật và thông báo</strong>
+              <strong>{t('updatesAndNotifications')}</strong>
             </p>
             <p>
-              &#8211;Chúng tôi sẽ thông báo cho quý khách qua email về các thông
-              báo thay đổi chính sách. Quý khách có thể hủy bỏ thông báo bất kỳ
-              lúc nào nếu quý khách không mong muốn nhận thư thông báo từ chúng
-              tôi.
+              &#8211;{t('youCanUnsubscribe')}
             </p>
             <p>
-              &#8211;Điều khoản này sẽ được thực hiện và hiệu lực từ ngày thông
-              báo.
+              &#8211;{t('thisClause')}
             </p>
             <p>
-              <strong>11. Các điều khoản khác</strong>
+              <strong>{t('miscellaneous')}</strong>
             </p>
             <p>
-              &#8211;Chúng tôi có quyền cập nhật lại các điều khoản này mà không
-              cần báo trước quý khách. Do đó, quý khách vui lòng kiểm tra thường
-              xuyên để cập nhật các thay đổi mới nhất.
+              &#8211;{t('weReserve')}
             </p>
             <p>
-              &#8211;Chúng tôi không chịu trách nhiệm pháp lý trong trường hợp
-              chậm trễ hoặc ngừng cung cấp dịch vụ do lý do bất khả kháng như
-              thiên tai, chiến tranh, biểu tình, pháp luật hoặc hành động của
-              chính phủ.
+              &#8211;{t('weAreYou')}
             </p>
             <p>
-              <strong>12. Giải quyết tranh chấp</strong>
+              <strong>{t('disputeResolution')}</strong>
             </p>
             <p>
-              &#8211;Trong trường hợp có tranh chấp xảy ra, vui lòng liên hệ với
-              chúng tôi qua email hoặc số điện thoại để được hỗ trợ tốt nhất.
+              &#8211;{t('inCaseOf')}
             </p>
             <p>
-              &#8211;Chúng tôi sẽ giải quyết tranh chấp nhanh chóng và hiệu quả
-              trong vòng 24 giờ.
+              &#8211;{t('24Hours')}
             </p>
           </div>
         </div>
